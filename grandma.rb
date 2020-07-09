40cba3f86624639ffd9e07d2334fc0f208895437
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-09 03:05:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,9 +7,3 @@ def speak_to_grandma(response)
     "NO, NOT SINCE 1938!"
   end
 end
-
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
