066ea1ea911402b664b507a50b60e74db919ad84
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-09 02:57:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,4 @@
 def speak_to_grandma(response)
-  if response == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
-  end
-end
-
-def speak_to_grandma(response)
   if response != response.upcase
     "HUH?! SPEAK UP, SONNY!"
   if response == "I LOVE YOU GRANDMA!"
